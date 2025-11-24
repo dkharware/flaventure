77d0dc458272b962f18b3fcc7a34bc90d10a0197
Deepak Kharware <dkharware@gmail.com>
2025-11-24 13:26:14 +0000

    shopify dropdown width increase so menus show properly

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -135,7 +135,7 @@ export default function Header() {
                 <NavigationMenuItem>
                   <NavigationMenuTrigger>Shopify</NavigationMenuTrigger>
                   <NavigationMenuContent>
-                    <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                    <ul className="grid w-[400px] gap-3 p-4 md:w-[600px] md:grid-cols-2 lg:w-[700px] ">
                       <li className="row-span-4">
                         <NavigationMenuLink asChild>
                           <a
