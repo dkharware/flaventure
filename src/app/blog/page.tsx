3af30d8881141a1ce4b4382e57c075cdeb4c720d
Deepak Kharware <dkharware@gmail.com>
2025-11-24 10:07:26 +0000

    and padding do .75rem

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -53,7 +53,7 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
   ];
 
   return (
-    <div className="container mx-auto py-8 px-4 md:py-12 md:px-6">
+    <div className="container mx-auto py-8 px-3 md:py-12 md:px-6">
         <Breadcrumbs items={breadcrumbItems} />
         <div className="lg:hidden fixed bottom-24 right-4 z-40">
             <Sheet>
