8b34cc9595269050188eb776a431cd2e0d401d97
Deepak Kharware <dkharware@gmail.com>
2025-12-06 06:25:09 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ src/app/next.config.ts
@@ -0,0 +1,48 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.shopify.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'khahax3ontgwrypo.public.blob.vercel-storage.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '5lgivccarqkvddiv.public.blob.vercel-storage.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  reactStrictMode: false,
+  experimental: {
+    allowedDevOrigins: [
+      "https://6000-firebase-shopifydevguide-1764137754571.cluster-ikxjzjhlifcwuroomfkjrx437g.cloudworkstations.dev"
+    ]
+  }
+};
+
+export default nextConfig;
